32a2ad5e95031a2d07b3c2942dda69f6854fd72a
xxAROX <arox.email.0@gmail.com>
2022-06-20 23:48:18 +0200

    updated copyright

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) Jan Sohn / xxAROX
+ * All rights reserved.
+ * I don't want anyone to use my source code without permission.
+ */
+
 require("colors");
 opt = require('node-getopt').create([
 	['v' , '', 'Show version']
